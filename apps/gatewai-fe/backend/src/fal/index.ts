3af6bf48f89028970fa7143f311c9a8a721dbc4b
okanasl <okn.aslnkn@gmail.com>
2025-12-31 12:33:15 +0300

    sex

--- apps/gatewai-fe/backend/src/fal/index.ts
+++ apps/gatewai-fe/backend/src/fal/index.ts
@@ -4,12 +4,12 @@
 // 	experimental_generateImage as generateImage,
 // } from "ai";
 // import fs from "fs";
-// 
+//
 // const { responses } = await experimental_generateSpeech({
 // 	model: fal.speech("fal-ai/flux/schnell"),
 // 	instructions: "A cat wearing a intricate robe",
 // });
-// 
+//
 // const filename = `image-${Date.now()}.png`;
 // fs.writeFileSync(filename, image.uint8Array);
 // console.log(`Image saved to ${filename}`);
