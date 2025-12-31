7cad83cbb9f5e8e3538943de798707b255900ae3
Okan Aslankan <okn.aslnkn@gmail.com>
2025-12-31 09:57:56 +0300

    lint imp

--- apps/gatewai-fe/src/routes/canvas/details/reactflow-container/index.tsx
+++ apps/gatewai-fe/src/routes/canvas/details/reactflow-container/index.tsx
@@ -4,7 +4,6 @@ import {
 	ConnectionMode,
 	type Edge,
 	type Node,
-	Panel,
 	ReactFlow,
 	SelectionMode,
 } from "@xyflow/react";
@@ -15,14 +14,10 @@ import { useAppDispatch, useAppSelector } from "@/store";
 import { setSelectedEdgeIds } from "@/store/edges";
 import { setSelectedNodeIds } from "@/store/nodes";
 import { selectRFEdges, selectRFNodes } from "@/store/rfstate";
-import { NodePalette } from "../../node-templates/node-palette";
 import { useCanvasCtx } from "../ctx/canvas-ctx";
 import { nodeTypes } from "../nodes";
 import { CustomConnectionLine, CustomEdge } from "../nodes/base";
 import { ReactFlowPanels } from "./panels";
-import { RightPanel } from "./right-panel";
-import { Toolbar } from "./toolbar";
-import { TopPanel } from "./top-panel";
 import { useZoomHotkeys } from "./use-zoom-hotkeys";
 
 const edgeTypes = {
