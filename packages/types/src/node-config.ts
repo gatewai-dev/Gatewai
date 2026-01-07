74e5019b5122a684625dba83271b3d86d1057ebe
okanasl <okn.aslnkn@gmail.com>
2026-01-07 13:52:39 +0300

    no transparent

--- packages/types/src/node-config.ts
+++ packages/types/src/node-config.ts
@@ -405,7 +405,6 @@ const VideoCompositorNodeConfigSchema = z
 		height: z.number().optional(),
 		background: z.string().optional(),
 		FPS: z.number().optional(),
-		isTransparent: z.boolean().optional().default(false),
 	})
 	.strict();
 // Main node schema
