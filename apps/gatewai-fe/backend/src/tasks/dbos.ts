fc89a6c9d928037ae0167aeabf941af7bc13fb91
okanasl <okn.aslnkn@gmail.com>
2026-01-09 10:35:10 +0300

    desk

--- apps/gatewai-fe/backend/src/tasks/dbos.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaDataSource } from "@dbos-inc/prisma-datasource";
-import { type PrismaClient, prisma } from "@gatewai/db";
-
-const dbosPrismaDataSource = new PrismaDataSource<PrismaClient>(
-	"app-db",
-	prisma,
-);
