45eaad31fe212b7d460268c5cd4da0bb7a9c0337
Okan Aslankan <okn.aslnkn@gmail.com>
2026-01-20 13:16:37 +0300

    blt

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
-export * from "../generated/client/client.js"; // Remove 'type' to export values too
-
+export * from "../generated/client/client.js";
+export * from "../generated/client/commonInputTypes.js";
+export * from "../generated/client/enums.js";
 // Export both types AND values from the generated client
 export * from "../generated/client/models.js";
 export { prisma } from "./client.js";
