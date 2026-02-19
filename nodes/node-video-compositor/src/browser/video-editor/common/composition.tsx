a2ec2d99ed1212ee204d76eb9acc51122ad635e7
Okan Aslankan <okn.aslnkn@gmail.com>
2026-02-19 21:56:24 +0300

    adjust word wrap

--- nodes/node-video-compositor/src/browser/video-editor/common/composition.tsx
+++ nodes/node-video-compositor/src/browser/video-editor/common/composition.tsx
@@ -258,7 +258,7 @@ export const CompositionScene: React.FC<SceneProps> = ({
 											: layer.verticalAlign === "bottom"
 												? "flex-end"
 												: "flex-start",
-									whiteSpace: "pre-wrap",
+									whiteSpace: "pre",
 								}}
 							>
 								{textContent}
